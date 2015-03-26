c35c8038d5b770bcc5ba0b22fcd964d56ec5da73
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2015-03-26 21:01:22 +0100

    Basic Class Structure Basic State Implemented

--- src/oose2015/SimpleSlickGame.java
+++ src/oose2015/SimpleSlickGame.java
@@ -3,26 +3,24 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import oose2015.states.GameplayState;
-import oose2015.states.MainMenuState;
+import oose2015.states.MainmenuState;
 import org.newdawn.slick.*;
-import org.newdawn.slick.state.GameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class SimpleSlickGame extends StateBasedGame {
-	public SimpleSlickGame(String gamename){
+public class SimpleSlickGame extends StateBasedGame
+{
+
+	public SimpleSlickGame(String gamename)
+	{
 		super(gamename);
 	}
 
-
     @Override
     public void initStatesList(GameContainer gameContainer) throws SlickException {
-        //creates gamestates
-        GameState mainMenu = new MainMenuState();
-        GameState gameRunning = new GameplayState();
 
-        //add the states
-        addState(mainMenu);
-        addState(gameRunning);
+        addState(new MainmenuState());
+        addState(new GameplayState());
+
     }
 
 	public static void main(String[] args)
@@ -30,7 +28,7 @@ public class SimpleSlickGame extends StateBasedGame {
 		try
 		{
 			AppGameContainer appgc;
-			appgc = new AppGameContainer(new SimpleSlickGame("Rouge Gauntlet"));
+			appgc = new AppGameContainer(new SimpleSlickGame("Simple Slick Game"));
 			appgc.setDisplayMode(640, 480, false);
 			appgc.start();
 		}
