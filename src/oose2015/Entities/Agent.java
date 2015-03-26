6209ca539e95972eff48efc82d0597373f0855d5
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2015-03-26 22:59:07 +0100

    basic class structure and state based

---
+++ src/oose2015/Entities/Agent.java
@@ -0,0 +1,14 @@
+package oose2015.entities;
+
+/**
+ * Created by @Kasper on 26/03/2015
+ * <p/>
+ * Description:
+ * ---
+ * <p/>
+ * Usage:
+ * ---
+ */
+
+public class Agent extends MoveableEntity {
+}
