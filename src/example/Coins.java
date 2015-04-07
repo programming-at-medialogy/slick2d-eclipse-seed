d65814972ad401bb2d67205b986a676450340dab
Theis Berthelsen <theisberthelsen@gmail.com>
2015-04-07 18:23:05 +0200

    Coin and grass tile

---
+++ src/example/Coins.java
@@ -0,0 +1,17 @@
+package example;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.GameContainer;
+public class Coins {
+
+	public Image coin = null;
+	
+	xpublic void GameContainer
+	
+	public void render (Graphics g){
+	
+	coin.draw("");
+	
+	}
+	
+}
