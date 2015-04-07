f63b084a5b93f48f1b7f32025d0107eb77c94f9c
katinal <dina.smed@gmail.com>
2015-04-07 15:59:33 +0200

    Base
    The start of the basic of our game

---
+++ src/example/Entity.java
@@ -0,0 +1,11 @@
+package example;
+
+public class Entity {
+	
+	int x, y;
+	public Entiry (int x, int y){
+		this.x= x;
+		this.y = y;
+	}
+
+}
---
+++ src/example/Main.java
@@ -0,0 +1,17 @@
+package example;
+
+import javax.swing.JFrame;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("MarioGame");
+		frame.setSize(800,600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.add(new GameFrame());
+		frame.setVisible(true);
+	}
+
+}
