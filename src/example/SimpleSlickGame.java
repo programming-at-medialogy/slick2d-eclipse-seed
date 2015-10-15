c01fdc46fc9eca8019f7c97f54d62bc5c6ec83e3
ThomasMacdonaldArnskov <thomas.macdonald.arnskov@gmail.com>
2015-10-15 16:51:25 +0200

    Et fint billede som baggrund :)

--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -6,16 +6,24 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Image;
+
 
 public class SimpleSlickGame extends BasicGame //My first comment
 {
+
+    Image bg;
+
 	public SimpleSlickGame(String gamename)
 	{
 		super(gamename);
 	}
 
 	@Override
-	public void init(GameContainer gc) throws SlickException {}//Comment 2
+	public void init(GameContainer gc) throws SlickException {
+
+        bg = new Image("assets/bg.png");
+    }//Comment 2
 
 	@Override
 	public void update(GameContainer gc, int i) throws SlickException {}
@@ -23,7 +31,8 @@ public class SimpleSlickGame extends BasicGame //My first comment
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-		g.drawString("Puerto Rico!", 250, 200);
+		//g.drawString("Pandemic!",300 , 200);
+        g.drawImage(bg,0,0);
 	}
 
 	public static void main(String[] args)
@@ -32,7 +41,7 @@ public class SimpleSlickGame extends BasicGame //My first comment
 		{
 			AppGameContainer appgc;
 			appgc = new AppGameContainer(new SimpleSlickGame("Simple Slick Game"));
-			appgc.setDisplayMode(1080, 720, false);
+			appgc.setDisplayMode(1100, 778, false);
 			appgc.start();
 		}
 		catch (SlickException ex)
