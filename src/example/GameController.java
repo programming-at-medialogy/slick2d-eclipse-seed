cabba6252ff4676cdc1f18b525107947a6c9c219
raminta.stanulionyte@gmail.com <RamiRami@192.168.0.13>
2015-10-21 19:41:39 +0200

    Added array list of card stack

--- src/example/GameController.java
+++ src/example/GameController.java
@@ -4,24 +4,31 @@ import java.util.*;
 
 public class GameController {
 
-ArrayList<Ability> abilityDb = new ArrayList<Ability>();
-ArrayList<Race>	raceDb = new ArrayList<Race>();
+	//Databases for races and abilities
+	ArrayList<Ability> abilityDb = new ArrayList<Ability>();
+	ArrayList<Race> raceDb = new ArrayList<Race>();
+
+	//The stack of races and abilities
+	ArrayList<ArrayList<Template>> stack = new ArrayList<ArrayList<Template>>();
 
 	public void initDb() {
-		
-		//Initialize abilities
-		abilityDb.add(new Ability("1",5,"fly","none"));
-		abilityDb.add(new Ability("2",5,"fly","none"));
-		abilityDb.add(new Ability("3",5,"fly","none"));
-		abilityDb.add(new Ability("4",5,"fly","none"));
-		
-		
-		//Initialize races 
-		raceDb.add(new Race("Amazon 1",8,"backup","conquer"));
-		raceDb.add(new Race("Amazon 2",8,"backup","conquer"));
-		raceDb.add(new Race("Amazon 3",8,"backup","conquer"));
-		raceDb.add(new Race("Amazon 4",8,"backup","conquer"));
-		
+
+		// Initialize abilities
+		abilityDb.add(new Ability("1", 5, "fly", "none"));
+		abilityDb.add(new Ability("2", 5, "fly", "none"));
+		abilityDb.add(new Ability("3", 5, "fly", "none"));
+		abilityDb.add(new Ability("4", 5, "fly", "none"));
+
+		// Initialize races
+		raceDb.add(new Race("Amazon 1", 8, "backup", "conquer"));
+		raceDb.add(new Race("Amazon 2", 8, "backup", "conquer"));
+		raceDb.add(new Race("Amazon 3", 8, "backup", "conquer"));
+		raceDb.add(new Race("Amazon 4", 8, "backup", "conquer"));
+
 	}
-	
+
+	public void createStack() {
+
+	}
+
 }
