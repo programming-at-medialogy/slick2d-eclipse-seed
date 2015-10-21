a54914821590971d47836e9ac71e0434650770b5
0x16e8bea <0x16e8bea@gmail.com>
2015-10-21 20:03:09 +0200

    Changed javadoc in Ability and Race.
    Added a class-specific javadoc
    Added a @see reference to the class-specific javadoc

--- src/example/Race.java
+++ src/example/Race.java
@@ -1,12 +1,24 @@
 package example;
 
+/**
+ * The Race class specifies each race in the database.
+ * 
+ * @param name
+ *            the name of the race
+ * @param unitAmount
+ *            the amount of units of given to the player
+ * @param effect
+ *            the effect of the race
+ * @param effectReq
+ *            the requirement for the effect to take place
+ */
 public class Race implements Template {
-	
+
 	String name;
 	int unitAmount;
-	
-	String effect; //Temporarily a 'String'
-	String effectReq; //Temporarily a 'String'
+
+	String effect; // Temporarily a 'String'
+	String effectReq; // Temporarily a 'String'
 
 	Race(String name, int unitAmount, String effect, String effectReq) {
 		this.name = name;
@@ -19,8 +31,9 @@ public class Race implements Template {
 	 * Returns the properties of the race card.
 	 * 
 	 * @return name, unitAmount, effect, effectReq
+	 * @see Race
 	 */
-	
+
 	public String debugStats() {
 		return "name: " + name + "\n" 
 				+ "unitAmount: " + unitAmount + "\n" 
