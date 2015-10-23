4875c73f6d52aa70a3c38ce968b2e98c44ee9b56
0x16e8bea <0x16e8bea@gmail.com>
2015-10-23 14:49:53 +0200

    Changed GameController class so it is more flexible
    Added addPair() and removePair() 
    Removed 2D array of Races and Abilities 
    Changed name of raceId to raceStack / abilityId to abilityStack 
    Changed debugStack()

--- src/example/GameController.java
+++ src/example/GameController.java
@@ -1,9 +1,6 @@
 package example;
 
-import java.lang.reflect.Array;
 import java.util.*;
-
-import com.sun.org.apache.bcel.internal.classfile.Code;
 
 /**
  *
@@ -17,90 +14,79 @@ public class GameController {
 	ArrayList<Race> raceDb = new ArrayList<Race>();
 	ArrayList<Ability> abilityDb = new ArrayList<Ability>();
 
-	// The stack of races and abilities
-	// ArrayList<Template> pair = new ArrayList<Template>();
-
-	// Template[] pair = new Template[2];
-	// ArrayList<Template[]> stack = new ArrayList<Template[]>();
-
+	// Pair consisting of a Race and an Ability
 	ArrayList<Pair> pairs = new ArrayList<Pair>();
 
 	// The id of races and abilities
-	ArrayList<Integer> raceId = new ArrayList<Integer>();
-	ArrayList<Integer> abilityId = new ArrayList<Integer>();
+	ArrayList<Race> raceStack = new ArrayList<Race>();
+	ArrayList<Ability> abilityStack = new ArrayList<Ability>();
 
 	public void initDb() {
 
 		// Initialize races
-		raceDb.add(new Race("Race 1", 8, "backup", "conquer"));
-		raceDb.add(new Race("Race 2", 8, "backup", "conquer"));
-		raceDb.add(new Race("Race 3", 8, "backup", "conquer"));
-		raceDb.add(new Race("Race 4", 8, "backup", "conquer"));
+		raceDb.add(new Race("Race 1", 8, "backup", "conquer")); 
+		raceDb.add(new Race("Race 2", 8, "backup", "conquer")); 
+		raceDb.add(new Race("Race 3", 8, "backup", "conquer")); 
+		raceDb.add(new Race("Race 4", 8, "backup", "conquer")); 
 
 		// Initialize abilities
 		abilityDb.add(new Ability("Ability 1", 5, "fly", "none"));
 		abilityDb.add(new Ability("Ability 2", 5, "fly", "none"));
 		abilityDb.add(new Ability("Ability 3", 5, "fly", "none"));
 		abilityDb.add(new Ability("Ability 4", 5, "fly", "none"));
+		abilityDb.add(new Ability("Ability 5", 5, "fly", "none"));
+		abilityDb.add(new Ability("Ability 6", 5, "fly", "none"));
+		abilityDb.add(new Ability("Ability 7", 5, "fly", "none"));
+		abilityDb.add(new Ability("Ability 8", 5, "fly", "none"));
 
 	}
 
 	public void initializeStack() {
 
 		for (int i = 0; i < raceDb.size(); i++) {
-			raceId.add(i);
+			raceStack.add(raceDb.get(i));
 		}
 
 		for (int i = 0; i < abilityDb.size(); i++) {
-			abilityId.add(i);
+			abilityStack.add(abilityDb.get(i));
 		}
 	}
 
 	/**
-	 * The {@code shuffle()} method is responsible for shuffling the race id's and ability id's before creating the stack.
+	 * The {@code shuffle()} method is responsible for shuffling the race id's
+	 * and ability id's before creating the stack.
 	 */
-	
-	public void shuffle() {
+	public void shuffleStack() {
+
 		Random r1 = new Random(Double.doubleToLongBits(Math.random()));
 		Random r2 = new Random(Double.doubleToLongBits(Math.random()));
-		
-		Collections.shuffle(raceId, r1);
-		Collections.shuffle(abilityId, r2);
+
+		Collections.shuffle(raceStack, r1);
+		Collections.shuffle(abilityStack, r2);
 	}
-
-	public void createStack() {
-		if (raceDb.size() > abilityDb.size()) {
-
-			for (int i = 0; i < abilityDb.size(); i++) {
-				pairs.add(new Pair(raceDb.get((int) raceId.get(i)), abilityDb.get((int) abilityId.get(i))));
-				pairs.add(new Pair(raceDb.get((int) raceId.get(i)), abilityDb.get((int) abilityId.get(i))));
-			}
-
-		} else {
-			for (int i = 0; i < raceDb.size(); i++) {
-				pairs.add(new Pair(raceDb.get((int) raceId.get(i)), abilityDb.get((int) abilityId.get(i))));
-				pairs.add(new Pair(raceDb.get((int) raceId.get(i)), abilityDb.get((int) abilityId.get(i))));
-
-			}
-		}
+	
+	public void addPair(Race race, Ability ability) {
+		raceStack.add(race);
+		abilityStack.add(ability);
+	}
+	
+	public void removePair() {
+		raceStack.remove(0);
+		abilityStack.remove(0);
 	}
 
 	public void debugStack() {
 		// list Race-id in the order it appears in the stack
 		System.out.println("Race: ");
-		for (int i = 0; i < raceDb.size(); i++) {
+		for (int i = 0; i < raceStack.size(); i++) {
 
-			System.out.println("Id: " + raceId.get(i) + " Name: " + raceDb.get((int) raceId.get(i)).name);
+			System.out.println("Id: " + i + " Name: " + raceStack.get(i).name);
 
 		}
 		// list Ability-id in the order it appears in the stack
 		System.out.println("Ability: ");
-		for (int i = 0; i < abilityDb.size(); i++) {
-
-			System.out.println("Id: " + abilityId.get(i) + " Name: " + abilityDb.get((int) abilityId.get(i)).name);
-
+		for (int i = 0; i < abilityStack.size(); i++) {
+			System.out.println("Id: " + i + " Name: " + abilityStack.get(i).name);
 		}
-
 	}
-
 }
