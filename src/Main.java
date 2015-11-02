f789b88ebf87fb8ad4c0d472da66ab35902239a0
lauris47 <lauris4747@gmail.com>
2015-11-02 12:31:11 +0100

    Hexagons works

--- src/Main.java
+++ src/Main.java
@@ -16,13 +16,15 @@ public class Main extends BasicGame{
 	int mostKnights; //the index of the player in the player-ArraList with the most knights
 	int thief; //the placement of the thief
 	int ownIndex; //This particular clients index in the player-array
-	ArrayList<Hexagon> hexagons; //ArrayList containing the Hexagon-objects that defines the game board
+	ArrayList [] hexagons = new ArrayList[20]; //ArrayList containing the Hexagon-objects that defines the game board
 	ArrayList<Harbour> harbours; //ArrayList containing the Harbour-object that are also part of the game board
 	ArrayList<Player> players; //ArrayList containing all the player-objects
 	NetworkHelper networkHelper;
 	boolean[] tradeRequest; //Do we need this?
 	boolean hasRequestedTrade; //Do we need this?
 	static boolean userInGame; 
+	static int scHeight = 1400;
+	static int scWidth = 900;
 	
 
 	private Image img = null;
@@ -31,24 +33,70 @@ public class Main extends BasicGame{
 	public Main(String gamename) {
 		// TODO Auto-generated constructor stub
 		super(gamename);
+		
+		//While-loop containing game flow
+		while(userInGame){
+			//check for user or server input
+			//draw graphics
+			//send commands to the server
+		    //receive updates about the game from the server 
+			//draw graphics
+		}
 
 	}
 	
 	@Override
 	public void init(GameContainer gc) throws SlickException {
-
-		 img = new Image("resources/cute-kitty.png");
+		//for(int i=0; i<hexagons.length; i++){
+			
+		 img = new Image("resources/hexagon.png");
+		//}
 	}
+	/*
+	public Vector2d rotate(double angle)
+	{
+		double xPrime = this.x*Math.cos(angle) - this.y*Math.sin(angle);
+		double yPrime = this.x*Math.sin(angle) + this.y*Math.cos(angle);
+		return new Vector2d(xPrime, yPrime);
+	}
+	*/
 
-
+	
 	@Override
 	public void update(GameContainer gc, int i) throws SlickException {}
 
+	
+
+	
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
 		
-		img.draw(0, 0);
+		float xLength = 150;
+		float yLength = 150;
+		float angle=0;
+		float x;
+		float y;
+		
+		for (int i = 0; i<20; i++){
+			
+	        x = (float) (xLength * Math.cos (angle));
+	        y = (float) (yLength * Math.sin (angle));
+	        
+			img.draw(x+720, y+400);
+			
+			if (i<5){
+				angle+=1.05;
+			} else{
+				angle +=0.45;
+			}
+			
+			if(i==5){
+				xLength += 100;
+				yLength += 100;
+				
+			}
+		}
 		//g.setBackground(Color.WHITE);
 		//g.drawString("Works", 250, 200);
 	}
@@ -109,25 +157,18 @@ public class Main extends BasicGame{
 		try
 		{
 			AppGameContainer game = new AppGameContainer(new Main("Settlers"));
-			game.setTargetFrameRate(60);
-			game.setMaximumLogicUpdateInterval(60);
-			game.setVSync(true);
-			game.setDisplayMode(640, 480, false);
-			game.start();
+			game.setTargetFrameRate(60); // framerate
+			game.setMaximumLogicUpdateInterval(60); // maximum framerate
+			game.setVSync(true); // Vertical sync
+			game.setDisplayMode(scHeight, scWidth, false); // sets screen size, false or true for full screen
+			game.start(); // to start Slick 2D
 		}
 		catch (SlickException ex)
 		{
 			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
 		}
 		
-		//While-loop containing game flow
-		while(userInGame){
-			//check for user or server input
-			//draw graphics
-			//send commands to the server
-		    //receive updates about the game from the server 
-			//draw graphics
-		}
+
 		
 	}
 }
