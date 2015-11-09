e1b1221f96205b0be270ef8a208101e3e1890e61
Chronim <nedimwz@gmail.com>
2015-11-09 13:34:25 +0100

    Owner enum 2

--- src/example/Owner.java
+++ src/example/Owner.java
@@ -1,6 +1,16 @@
 package example;
 
 public enum Owner {
-	BOARD, PLAYER1, PLAYER2, PLAYER3, PLAYER4, PLAYER5;
+	BOARD (0), PLAYER_1 (1), PLAYER_2 (2), PLAYER_3 (3), PLAYER_4 (4), PLAYER_5 (5);
+	
+	private int identity;
+	
+	Owner(int identity) {
+		this.identity = identity;
+	}
+	
+	int getIdentity() {
+		return identity;
+	}
 
 }
