a4c675a5abc63480e8964b5cf06b296501654d1b
Frederik Emil <frederik.e.laursen@gmail.com>
2015-11-12 13:47:53 +0100

    Revert "Updated methods"
    This reverts commit 441202d4fe64661ed97ad71666531aec149bbbcb.

--- src/Road.java
+++ src/Road.java
@@ -10,8 +10,6 @@ public class Road {
 	static ArrayList<Road> roads = new ArrayList<Road>();
 	boolean startTouch;
 	boolean endTouch;
-	boolean visited;
-	
 	static int longestRoad = 0;
 	
 	Road(Position startPos, Position endPos,int playerIndex){
@@ -20,8 +18,7 @@ public class Road {
 		start = startPos;
 		end = endPos;
 		player = playerIndex;
-		checkEnds(this);
-		visited = false;
+		checkEnds();
 	}
 	
 	public static void buildRoad(Position startPos, Position endPos,int playerIndex){
@@ -46,13 +43,13 @@ public class Road {
 		int thisRoad = 0;
 
 		for(int i = 0; i<Road.roads.size(); i++){
-			tempLength = 0;
+			tempLength = 1;
 			if(Road.roads.get(i).player == player){ // we only want to look at this players roads
-				if(Road.roads.get(i).endTouch == true){
-					thisRoad = roadLength(Road.roads.get(i),"end");
-				}
-				else if(Road.roads.get(i).startTouch == true){
+				if(Road.roads.get(i).endTouch == false){
 					thisRoad = roadLength(Road.roads.get(i),"start");
+				}
+				if(Road.roads.get(i).startTouch == false){
+					thisRoad = roadLength(Road.roads.get(i),"end");
 				}
 			}	
 		}		
@@ -65,75 +62,67 @@ public class Road {
 	}
 	
 	
-	
 	static int roadLength(Road theRoad, String endOrStart){
 		for (int i = 0; i < Road.roads.size(); i++){
 			if (theRoad != Road.roads.get(i)){ 
-				
 				if(endOrStart == "end"){
 					if(Position.comparePosition(theRoad.end, Road.roads.get(i).start)){
+						System.out.println("test");
 						tempLength ++;
-						roadLength(Road.roads.get(i), "en");
+						roadLength(Road.roads.get(i),"end");
 					}
-					if(Position.comparePosition(theRoad.end, Road.roads.get(i).end)){
+					else if(Position.comparePosition(theRoad.end, Road.roads.get(i).end)){
+						System.out.println("test");
 						tempLength ++;
-						roadLength(Road.roads.get(i), "start");
+						roadLength(Road.roads.get(i),"start");
 					}
-					else{
-						if (tempLength >= roadLength){
-							roadLength = tempLength;
-							System.out.println(roadLength);
-						}
-						tempLength --;
+					else if (tempLength > roadLength){
+						roadLength = tempLength;
 					}
 				}
 				
-				if(endOrStart == "start"){
+				else if(endOrStart == "start"){
 					if(Position.comparePosition(theRoad.start, Road.roads.get(i).start)){
+						System.out.println("test");
 						tempLength ++;
-						roadLength(Road.roads.get(i), "end");
+						roadLength(Road.roads.get(i),"end");
 					}
 					if(Position.comparePosition(theRoad.start, Road.roads.get(i).end)){
+						System.out.println("test");
 						tempLength ++;
-						roadLength(Road.roads.get(i), "start");
+						roadLength(Road.roads.get(i),"start");
+						//System.out.println(tempLength);
 					}
-					}
-					else{
-						if (tempLength >= roadLength){
-							roadLength = tempLength;
-							System.out.println(roadLength);
-						}
-						tempLength --;
+					else if (tempLength > roadLength){
+						roadLength = tempLength;
+						//System.out.println(roadLength);
 					}
 				}
-					
-					
+			}
 		}
 		return roadLength;
 	}
 	
 	
-	
-	void checkEnds(Road theRoad){
+	void checkEnds(){
 		for(int i = 0; i<Road.roads.size(); i++){
-			if(theRoad != Road.roads.get(i)){
-				if(Position.comparePosition(theRoad.end, Road.roads.get(i).start)){
-					endTouch = true;
-					Road.roads.get(i).startTouch = true;
-				}
-				if(Position.comparePosition(theRoad.end, Road.roads.get(i).end)){
-					endTouch = true;
-					Road.roads.get(i).endTouch = true;
-				}
-				if(Position.comparePosition(theRoad.start, Road.roads.get(i).start)){
-					startTouch = true;
-					Road.roads.get(i).startTouch = true;
-				}
-				if(Position.comparePosition(theRoad.start, Road.roads.get(i).end)){
-					startTouch = true;
-					Road.roads.get(i).endTouch = true;
-				}
+			if(end == Road.roads.get(i).start){
+				endTouch = true;
+				Road.roads.get(i).startTouch = true;
 			}
+			if(end == Road.roads.get(i).end){
+				endTouch = true;
+				Road.roads.get(i).endTouch = true;
+			}
+			if(start == Road.roads.get(i).start){
+				startTouch = true;
+				Road.roads.get(i).startTouch = true;
+			}
+			if(start == Road.roads.get(i).end){
+				startTouch = true;
+				Road.roads.get(i).endTouch = true;
+			}
+			
 		}
 	}
 
