85ca25ab288588c873c004b73e19c65abc3f7890
lauris47 <lauris4747@gmail.com>
2015-11-13 19:39:01 +0100

    Dynamic setup
    Graphics size can be changed with one variable scFactor, the rest will
    adapt to changes.

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import com.google.gson.Gson;
-import org.newdawn.slick.Color;
+
 
 
 public class Main extends BasicGame{
@@ -25,12 +26,21 @@ public class Main extends BasicGame{
 	boolean hasRequestedTrade; //Do we need this?
 	static boolean userInGame; 
 	
+	
+	
+	private Image[] img = new Image[19]; // Array for hexagon images
 	static int scWidth = 1400; // Game screen width
 	static int scHeight = 900; // Game screen height
-	
-	
-	private Image[] img= new Image[19]; // Array for hexagon images
 
+	float diameter; // diameter for hexagon placement
+	boolean hexReady = false; // NOT USED NOW, discuss! To draw hexagons once in rendered. If not used remove // NOT USED NOW, discuss!
+	
+	float xPos; // hexagon position X
+	float yPos; // hexagon position Y
+	float angle; // hexagon angle
+	
+	float hexHeight; // to get height of hexagon
+	float hexWidth; // to get width of hexagon
 	
 	public Main(String gamename) { // inputs game name from slick2d
 		super(gamename); // to get game name
@@ -44,12 +54,10 @@ public class Main extends BasicGame{
 		    //receive updates about the game from the server 
 			//draw graphics
 		}
-
 	}
 	
 	@Override
 	public void init(GameContainer gc) throws SlickException { // to initialize elements
-		
 		// to initialize multiple images
 		for(int i=0; i<hexagons.length; i++){ //goes trough hexagon array
 			img[i] = new Image("resources/hexagon_" + (i) + ".png"); //Assigns every hexagon a name
@@ -57,51 +65,62 @@ public class Main extends BasicGame{
 	}
 	
 	@Override
-	public void update(GameContainer gc, int i) throws SlickException {
-		
+	public void update(GameContainer gc, int i) throws SlickException {	
 	}
-
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-		// move up!
-		Color bkColor = Color.decode("#e5cea1");
-		g.setBackground(bkColor);
-		float diameter = 90;
-		float angle = 0;
-		float xPos;
-		float yPos;
-		// Move up!
+		Color bkColor = Color.decode("#e5cea1"); // create custom color
+		g.setBackground(Color.black); // set background color
 		
-		for (int i = 0; i<hexagons.length; i++){
-			// center hexagon
-			if(i==0){
-				img[i].draw(scWidth/2, scHeight/2);
-			}
-			//middle circle of hexagons
-			if (i>0 && i<7){
-				angle+=1.05;
-				xPos = (float) (diameter * Math.cos (angle));
-				yPos = (float) (diameter * Math.sin (angle));	      
-				img[i].draw(xPos+scWidth/2, yPos+scHeight/2);				
-			} 
-			// outer circle of hexagons
-			if(i>6){
-				if(i%2!=0){
-					diameter = 156;
-				}else{
-					diameter = 180;
+		// move out, since used only once in setup
+		float scFactor = 0.4f; // Dynamic setup: scales images according this value 
+		float padding = img[0].getWidth()/22*scFactor; // Dynamic setup: space between polygons
+		diameter = (img[0].getWidth()+ padding)*scFactor; // Dynamic setup: diameter according image width, + padding for space in between
+
+		//end. move out, since used only once in setup
+		
+			for (int i = 0; i<hexagons.length; i++){
+				// center hexagon
+	
+				if(i==0){
+					img[i].draw(scWidth/2-img[0].getWidth()/2*scFactor, scHeight/2-img[0].getHeight()/2*scFactor, scFactor); // draw center hexagon
 				}
-				angle +=0.524;
-		        xPos = (float) (diameter * Math.cos (angle));
-		        yPos = (float) (diameter * Math.sin (angle));	      
-				img[i].draw(xPos+scWidth/2, yPos+scHeight/2);		
+				//middle circle of hexagons
+				
+				if (i>0 && i<7){
+					xPos = (float) (diameter * Math.cos (angle(i))); // draw hexagon in curcular manner
+					yPos = (float) (diameter * Math.sin (angle(i)));	      
+					img[i].draw(xPos+scWidth/2-img[0].getWidth()/2*scFactor, yPos+scHeight/2-img[0].getHeight()/2*scFactor, scFactor);		
+					//myImage.draw(55, 66, 1.2f);
+				} 
+				// outer circle of hexagons
+				if(i>6){
+					if(i%2!=0){
+						diameter = ( ((img[0].getWidth()*2)+padding) - img[0].getWidth()/4f )*scFactor; // Dynamic setup : diameter for outer polygons according image width. 3.8 is ~third of image that gets inside the cricle
+						//diameter = 156;
+					}else{
+					diameter = (img[0].getWidth()*2)*scFactor+padding; // Dynamic setup : diameter for outer polygons according image width
+					}
+					//angle +=0.524f;
+					 
+					
+			        xPos = (float) (diameter * Math.cos (angle(i))); 
+			        yPos = (float) (diameter * Math.sin (angle(i)));	      
+					img[i].draw(xPos+scWidth/2-img[0].getWidth()/2*scFactor, yPos+scHeight/2-img[0].getHeight()/2*scFactor, scFactor);	
+				}
+				//g.drawString("Text", 250, 200);
 			}
-			
-
-			//g.drawString("Works", 250, 200);
 		}
-
+	// Function to calculate angle for each polygon
+	private float angle(int hexIndex){
+		if (hexIndex <=6){
+			// for inner
+			angle  = hexIndex * 1.048f; // 1.05f is specific angle for all polygons up to 7th.
+		}else
+			// for outer
+			angle  = hexIndex * 0.524f; // 0.524 for hexagons from 7th
+		return angle;
 	}
 
 	//Methods for initial phase
