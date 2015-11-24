e2e1e0de790bbf4d81c4ba16acc7c5c9f2da0a6d
ThatGuyWithTheBeard <nicklas.kristoffersen@hotmail.com>
2015-11-24 16:21:57 +0100

    Server

--- src/NetworkServer.java
+++ src/NetworkServer.java
@@ -1,48 +1,94 @@
-
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class NetworkServer {
-	
-	private static final int PORT = 82;
-	
-	private static String sentence;
-	private static String tempSentence;
+public class NetworkServer  {
 	
 	private static ServerSocket serverSocket;
-	private static Socket clientLink;
-	private static Scanner socketReader;
+	private static final int PORT = 0;
+	private static ArrayList<ClientHandler> handlers;
 	
-	private static PrintWriter serverOutput;
+	public static void startServer() throws IOException {
+		handlers = new ArrayList();
+		
+		try {
+			serverSocket = new ServerSocket(PORT);
+		} catch (IOException ioEx) {
+			System.out.println("\nUnable to set up port!");
+			System.exit(1);
+		}
+		
+		do {
+			// wait for client
+			System.out.println("\nListening on port " + serverSocket.getLocalPort());
+			System.out.println("Server Ip Address: " + InetAddress.getLocalHost().getHostAddress());
+			Socket client = serverSocket.accept();
+			System.out.println("\nNew client accepted.\n");
 	
-	public static void main(String args[]) throws IOException {
-		
-		serverSocket = new ServerSocket(PORT);
-		clientLink = serverSocket.accept();
-		
-		System.out.println("The server has now been opened.");
-		System.out.println("Waiting for input...");
-		
-		socketReader = new Scanner(clientLink.getInputStream());
-		sentence = socketReader.nextLine();
-		
-		/*
-		System.out.println("A new number has been accepted from the client: " + num);
-		System.out.println("Performing mathematical operation...");
-		*/
-		
-		tempSentence = sentence.toUpperCase();
-		
-		System.out.println("The new sentence is now: " + tempSentence);
-		
-		serverOutput = new PrintWriter(clientLink.getOutputStream());
-		serverOutput.println(tempSentence);
-		serverOutput.flush();
-		
+			handlers.add(new ClientHandler(client, handlers.size()));
+			handlers.get(handlers.size() - 1).start();
+			ServerMain.addPlayer(handlers.size() - 1);
+		} while (true);
 	}
+	
+	public static void send(int id, String message) {
+		handlers.get(id).send(message);
+	}
+	
+	public static void sendToAll(String message) {
+		for (ClientHandler handler : handlers)
+			handler.send(message);
+	}
+}
 
+class ClientHandler extends Thread {
+	
+	private Socket client;
+	private Scanner input;
+	private PrintWriter output;
+	private final int id;
+	private static final String exitCode = "QUIT";
+	
+	public ClientHandler(Socket socket, int id) {
+		client = socket;
+		try {
+			input = new Scanner(client.getInputStream());
+			output = new PrintWriter(
+			client.getOutputStream(),true);
+		} catch(IOException ioEx) {
+			ioEx.printStackTrace();
+		}
+		
+		this.id = id;
+	}
+	
+	public void run() {
+		
+		String received;
+		do {
+			received = input.nextLine();
+			ServerMain.received(id, received);
+		} while (!received.equals(exitCode));
+		
+		try {
+			if (client!=null) {
+				System.out.println("Closing down connection…");
+				client.close();
+			}
+		} catch(IOException ioEx) {
+			System.out.println("Unable to disconnect!");
+		}
+	}
+	
+	public void send(String message) {
+		
+		try {
+			System.out.println("Sending: " + message + "\nTo: " + client.getInetAddress().getHostAddress());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		output.println(message);
+	}
 }
--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,6 +1,25 @@
+import java.io.IOException;
+
 public class ServerMain {
 
 	public static void start() {
 		System.out.println("Server is running");
+		try {
+			NetworkServer.startServer();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void received(int clientId, String message) {
+		//NetworkServer.sendToAll("Message received from " + clientId + ":\n" + message);
+		/*if (GameData.players.get(clientId).getPlayerName() == null) {
+			GameData.players.get(clientId).setPlayerName(message);
+		}*/
+	}
+	
+	public static void addPlayer(int id) {
+		//GameData.players.add(new Player(id));
 	}
 }
