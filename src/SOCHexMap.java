767b91778385732b8e40a4a8ae46874a16a07b72
Polsenols <mads_did@hotmail.com>
2015-11-25 13:28:49 +0100

    Hexmap for socket
    class used to generate hexmap

---
+++ src/SOCHexMap.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class SOCHexMap {
+
+    int[] tileIndex;
+    int[] numberIndex;
+
+    SOCHexMap(){
+
+        tileIndex = new int[] {0,0,0,1,1,1,1,2,3,3,3,3,4,4,4,5,5,5,5};
+        numberIndex = new int[]{2,2,3,3,4,4,5,5,6,8,8,9,9,10,10,11,11,12,6};
+        shuffleArray(tileIndex);
+        shuffleArray(numberIndex);
+
+    }
+
+
+
+    public void shuffleArray(int[] ar){
+        Random rnd = ThreadLocalRandom.current();
+        for (int i = ar.length - 1; i > 0; i--)
+        {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            int a = ar[index];
+            ar[index] = ar[i];
+            ar[i] = a;
+        }
+    }
+
+}
