95a35c17f948021d0d6ef33ba6a0fee134203ace
lauris47 <lauris4747@gmail.com>
2015-11-26 21:21:21 +0100

    Two more resource cards graphics

--- src/Main.java
+++ src/Main.java
@@ -74,8 +74,8 @@ public class Main extends BasicGame{ //Is not the actually main.
 		for (int n=2; n<=12; n++){
 			numImg[n-2] = new Image("resources/numImg_" + (n) + ".png"); //initializing number images
 		}
-		for (int c=0; c<5; c++){
-			crdImg[c] = new Image ("resources/r_card_1.jpg");
+		for (int c=0; c<3; c++){
+			crdImg[c] = new Image ("resources/r_card_"+(c)+".jpg");
 		}
 		
 		robImg = new Image("resources/robber.png");
@@ -165,7 +165,7 @@ public class Main extends BasicGame{ //Is not the actually main.
 			}else
 				hexImg[6].draw(xPos + scWidth/2-hexImg[0].getWidth()/2*scFactor, yPos + scHeight/2-hexImg[0].getHeight()/2*scFactor, scFactor);
 		}
-		for (int c = 0; c<5;  c++){ // to display resource cards/ number 5 has to be replaced with number of cards player has 
+		for (int c = 0; c<3;  c++){ // to display resource cards/ number 5 has to be replaced with number of cards player has 
 			crdImg[c].draw(crdPosX+(scWidth/2-crdWidth/2*scFactor), crdPosY+(scHeight-crdHeight*scFactor)-crdHeight/100, scFactor);
 			crdPosX = cardPosition(c, 5); // 5 is to change - amount of resource cards
 		}
