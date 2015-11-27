c3dbf4380ba7a086eb9714823233282ae66a3d5e
aishahhussain <ahussa14@student.aau.dk>
2015-11-27 13:28:34 +0100

    Created the Elve class
    Made the elve class
    Wrote what it should do in sudo code

---
+++ src/example/Elves.java
@@ -0,0 +1,11 @@
+package example;
+
+public class Elves {
+/*
+ if enemy has concurred your regions
+ take tokens into hand for redeployment
+ at the end of the current players turn
+ (it does not discard a token into the tray as you would normally do) 
+ 
+ */
+}
