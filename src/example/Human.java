18c9a69294fcf4ee753d96b4eb2b2e9c01c2039b
aishahhussain <ahussa14@student.aau.dk>
2015-11-27 13:29:49 +0100

    Made the human class
    Made the human class
    Wrote what it should do in sudo-code

--- src/example/Human.java
+++ src/example/Human.java
@@ -1,5 +1,16 @@
 package example;
 
-public class Human {
 
+public class Human extends Base {
+
+/*
+ Needs a name : string
+ Needs owned regions : int
+ Needs to know something about the player regions 
+ If  the regions in the players owned regions are farmlands t
+ recieve 1 victory coin
+ */
 }
+	
+
+
