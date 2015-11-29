8ee7032a1f61bdaa0102739ec14e6ad8128ac7c7
litvinas <melynasdangus@gmail.com>
2015-11-29 19:25:08 +0100

    Created the Die class
    The result() method prints out the number the die has rolled.

---
+++ src/example/Die.java
@@ -0,0 +1,22 @@
+package example;
+
+import java.util.Collections;
+import java.util.Random;
+
+public class Die {
+	
+	
+	public static int roll() {
+		int[] sides = {0, 0, 0, 1, 2, 3};
+		int result = new Random().nextInt(sides.length);
+		return sides[result];
+	}
+
+	public static void result() {
+		System.out.println("You roll the reinforcement die. Number rolled: " + roll());		
+	}
+	
+	public static void main(String[] args) { 		// coz we dont have a main class right now
+		result();
+	}
+}
