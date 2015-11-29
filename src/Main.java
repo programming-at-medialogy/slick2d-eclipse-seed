41786bac2d467da362eec11a003898a7cf1e37a5
lauris47 <lauris4747@gmail.com>
2015-11-29 15:58:06 +0100

    Dice Graphics

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.BasicGame;
@@ -8,8 +6,6 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
-import com.google.gson.Gson;
-
 
 
 public class Main extends BasicGame{ //Is not the actually main.
