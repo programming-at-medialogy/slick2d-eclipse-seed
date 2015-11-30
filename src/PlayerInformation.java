834c9d940051ab064405395ad8967482d2a2fb29
Polsenols <mads_did@hotmail.com>
2015-11-30 12:41:24 +0100

    Update playerinformation class

--- src/PlayerInformation.java
+++ src/PlayerInformation.java
@@ -5,8 +5,15 @@ public class PlayerInformation implements Serializable{
 
     boolean playerOneReady, playerTwoReady, playerThreeReady, playerFourReady;
     int playerNumber;
+    int[] houseColour = new int[54];
+    int[] roadsColourDiagonal = new int[72];
+    int[] roadsColourStraight = new int[72];
     int[] SOCtileIndex = new int[19];
     int[] SOCnumberIndex = new int[19];
+    boolean[] SOChouseArea = new boolean[54];
+    boolean[] SOCroadAreaDiagonal = new boolean[72];
+    boolean[] SOCroadAreaStraight = new boolean[72];
+   
 
 
     PlayerInformation(){
@@ -14,5 +21,7 @@ public class PlayerInformation implements Serializable{
         playerTwoReady = false;
         playerThreeReady = false;
         playerFourReady = false;
+        
+        
     }
 }
