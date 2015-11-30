2e8c4cffc3f444040dbb3c1c03737acf3b2e9390
Mathias <mathias_clemme@hotmail.com>
2015-11-30 11:27:17 +0100

    Each player can only write to one console
    When pressing the Roll Dice button the roll is only displayed in the
    players field.

--- src/OnScreenTextField.java
+++ src/OnScreenTextField.java
@@ -1,6 +1,3 @@
-
-
-
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -8,8 +5,6 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.gui.TextField;
 
 public class OnScreenTextField {
-
-	private int once = 0;
 
 	TextField textField, textField2, textField3, textField4;
 	OnScreenButton diceButton;
@@ -23,31 +18,44 @@ public class OnScreenTextField {
 	}
 
 	public void create(GameContainer gc) {
-		/*textField = new TextField(gc, gc.getDefaultFont(), 990, 10, 200, 300);
-		textField.setBorderColor(Color.white);
-		textField.setBackgroundColor(Color.gray);*/
+
 		textField = new TextField(gc, gc.getDefaultFont(), 990,10,200,35);
-		textField.setBorderColor(Color.red);
+		textField.setBorderColor(Color.green);
 		textField.setBackgroundColor(Color.gray);
 		
 		textField2 = new TextField(gc, gc.getDefaultFont(), 990,50,200,35);
-		textField2.setBorderColor(Color.blue);
+		textField2.setBorderColor(Color.red);
 		textField2.setBackgroundColor(Color.gray);
 		
 		textField3 = new TextField(gc, gc.getDefaultFont(), 990,90,200,35);
-		textField3.setBorderColor(Color.green);
+		textField3.setBorderColor(Color.blue);
 		textField3.setBackgroundColor(Color.gray);
 		
 		textField4 = new TextField(gc, gc.getDefaultFont(), 990,130,200,35);
-		textField4.setBorderColor(Color.white);
+		textField4.setBorderColor(Color.magenta);
 		textField4.setBackgroundColor(Color.gray);
 	}
 
 	public void writeDiceToConsole() {
 		//depending on how we recognize players, this much change accordingly
-		textField.setConsumeEvents(true);
-		textField.setText(Integer.toString(dice.rollDice()));
-		textField.getText();
+		if (control.playerNo == 1){
+			textField.setConsumeEvents(true);
+			textField.setText(Integer.toString(dice.rollDice()));
+			textField.getText();
+		}else if (control.playerNo == 2){
+			textField2.setConsumeEvents(true);
+			textField2.setText(Integer.toString(dice.rollDice()));
+			textField2.getText();
+		}else if (control.playerNo == 3){
+			textField3.setConsumeEvents(true);
+			textField3.setText(Integer.toString(dice.rollDice()));
+			textField3.getText();
+		}else if (control.playerNo == 4){
+			textField4.setConsumeEvents(true);
+			textField4.setText(Integer.toString(dice.rollDice()));
+			textField4.getText();
+		}
+		
 	}
 
 	public void render(GameContainer gc, Graphics g) throws SlickException {
@@ -59,6 +67,7 @@ public class OnScreenTextField {
 
 	public void update(GameContainer gc, int i) throws SlickException {
 		diceButton.update(gc, i);
+		int once = 0;
 
 		if (control.isPlayerTurn == true && diceButton.buttonDiceControl == true) {
 			if (once < 1) {
