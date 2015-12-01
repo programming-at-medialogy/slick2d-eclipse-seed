90b24b7f6b2a2c6e15996c594195065142821abf
lauris47 <lauris4747@gmail.com>
2015-12-01 15:46:22 +0100

    Conflict solve

--- src/GameState.java
+++ src/GameState.java
@@ -121,6 +121,14 @@ public class GameState extends BasicGameState implements KeyListener {
 				diceNumber(2);	
 			}
 		};
+		TextBox chatInput = new TextBox(0, Windows.scHeight - 35, (int)(1200*Windows.scFactor), 35, 20, this) {
+			@Override
+			public void onSubmit() {
+				
+			}
+		};
+		
+		ListBox chatOutput = new ListBox(0, Windows.scHeight - 35 - 300, (int)(1200*Windows.scFactor), 300, 20, this);
 	}
 
 
@@ -154,6 +162,9 @@ public class GameState extends BasicGameState implements KeyListener {
 		diceImg[Dice.dice1-1].draw((int) Math.random()*(Windows.scWidth-hexWidth*Windows.scFactor*2+diceImg[1].getWidth())+(Windows.scWidth-hexWidth*Windows.scFactor*2)          ,Windows.scHeight/2 , Windows.scFactor*0.8f);
 		diceImg[Dice.dice2-1].draw(Windows.scWidth-hexWidth*Windows.scFactor*2-diceImg[1].getWidth()*Windows.scFactor ,Windows.scHeight/2, Windows.scFactor*0.8f);
 		Button.draw(g, this);
+		
+		ListBox.draw(g, this);
+		TextBox.draw(g, this);
 	}
 	
 
@@ -190,6 +201,8 @@ public class GameState extends BasicGameState implements KeyListener {
 		for (int d=0; d<5; d++){
 			devCrdImg[d].draw(Windows.scWidth-crdWidth*Windows.scFactor, Windows.scHeight-crdHeight*Windows.scFactor, Windows.scFactor);
 		}
+		ListBox.update(this);
+		TextBox.update(this);
 
 	}
 	
