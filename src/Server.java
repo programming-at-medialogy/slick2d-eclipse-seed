a81f482827a837af0f70ae50ea1699676f9febd1
Motusification <mga94@live.dk>
2015-12-03 20:39:00 +0100

    Comment
    Server comments

--- src/Server.java
+++ src/Server.java
@@ -8,47 +8,46 @@ import java.util.List;
 public class Server {
 	
     protected ServerSocket serverSocket = null;
-    protected boolean listening = true;
-    protected List<ServerThread> clientSockets;
-    PlayerInformation serverData;
-    int playerNumber = 1;
-    String serverIP = "192.168.43.235";
+    protected boolean listening = true; //creates an infinite loop
+    protected List<ServerThread> clientSockets; //creates a list of serverThreads.
+    PlayerInformation serverData; 
+    int playerNumber = 1; //sets the starting player number of the first player that connects
+    String serverIP = "192.168.43.235"; //IP of the server
     InetAddress addr = InetAddress.getByName(serverIP);
     
-    SOCHexMap map;
+    SOCHexMap map; 
 
     public Server(int port) throws IOException {
-        serverSocket = new ServerSocket(5555);
-        clientSockets = new ArrayList<>();
-        map = new SOCHexMap();
+        serverSocket = new ServerSocket(5555,55,addr); //creates serversocket at given port and IP.
+        clientSockets = new ArrayList<>(); //initialize array list of serverThreads.
+        map = new SOCHexMap(); //initialize the HexMap, which is what we use to shuffle indexes of tiles and numbers.
 
-        while (listening) {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Client connected: " + clientSocket);
-            clientSockets.add(new ServerThread(clientSocket, this));
-            serverData = new PlayerInformation();
-            handlePlayer(serverData);
+        while (listening) { //while true
+        	
+            Socket clientSocket = serverSocket.accept(); //accept incoming sockets. Anything below this will be ignored until a client connects.
+            System.out.println("Client connected: " + clientSocket); //prints out the connected client.
+            clientSockets.add(new ServerThread(clientSocket, this)); //add the client to a new serverThread
+            serverData = new PlayerInformation(); //object used to send out the player number to each player, and the HexMap array.
+            handlePlayer(serverData); //handle the player, which gives player number and array.
         }
 
         serverSocket.close();
     }
 
-    public void broadcastObject(Object obj){
-        for(ServerThread thread : clientSockets) {
+    ///function used to broadcast everything the server receives to all clients///
+    public void broadcastObject(Object obj){ 
+        for(ServerThread thread : clientSockets) { //for each thread, write object to client in thread.
             thread.write(obj);
         }
     }
-
-    public void removeSocket(Socket socket) {
-        clientSockets.remove(socket);
-        System.out.println("socket removed");
-    }
-
+    
+    ///stores the number and tile indexes in the two arrays
     public void generateMap(PlayerInformation server){
         server.SOCnumberIndex = map.numberIndex;
         server.SOCtileIndex = map.tileIndex;
     }
-
+    
+    //handle the player by giving them a player number.
     public void handlePlayer(PlayerInformation server){
         generateMap(server);
         server.playerNumber = playerNumber;
@@ -56,7 +55,9 @@ public class Server {
         playerNumber++;
     }
 
+    
     public static void main(String[] args) throws IOException {
+    	//starts the server
         new Server(4444);
     }
 	
