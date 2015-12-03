b8c5bfaa4c91a7e07a7b79ecb9809668a0742b54
Kallabow <kallabow2@gmail.com>
2015-12-03 16:07:41 +0100

    Create Server.java
    Moving from pandemic/server, konstantin's bit

---
+++ src/gameEngine/Server.java
@@ -0,0 +1,38 @@
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+
+	static ServerSocket serverSocket;
+	static Socket socket;
+	static DataOutputStream out;
+	static DataInputStream in;
+	static Users[] user = new Users[10];
+	
+	public static void main (String[] args) throws Exception 
+	{
+		System.out.println("Starting server...");
+		serverSocket = new ServerSocket(7777);
+		System.out.println("Server is running!");
+		
+		while(true)
+		{
+		socket = serverSocket.accept();
+		for (int i = 0; i < 10; i++) 
+			{
+				System.out.println("Connected from: " + socket.getInetAddress());
+				out = new DataOutputStream(socket.getOutputStream());
+				in = new DataInputStream(socket.getInputStream()); 
+					if (user[i] == null) 
+						{
+							user[i] = new Users(out, in, user);	
+							Thread thread = new Thread(user[i]);
+							thread.start(); 
+							break;
+						}
+		
+			}
+		}
+	}
+}
