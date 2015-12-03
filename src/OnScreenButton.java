bf1f54c14c4b2a5ba4aaf619bfdb2cda0b53886a
MartinSorensen <martinsorensen1992@gmail.com>
2015-12-03 20:25:08 +0100

    Comments and small change

--- src/OnScreenButton.java
+++ src/OnScreenButton.java
@@ -6,18 +6,25 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
+/*
+ * This class renders images on the screen and 
+ * checks to see if these images (buttons) have been clicked.
+ */
 public class OnScreenButton {
 	
 	int screenWidth = 1200;
 	int screenHeight = 700;
 	
+	//Instances of different classes.
 	Controller control;
 	Game game;
 	SettlementSpawn spawn;
 	Trade trade;
 	
+	//A string to display that it is now your turn.
     String yourTurnString = "It is your turn!";
 	
+    //Instances of the OnScreenButtonSpawn class, used to render images.
 	OnScreenButtonSpawn buttonPlaceHouse;
 	OnScreenButtonSpawn buttonPlaceCity;
 	OnScreenButtonSpawn buttonPlaceRoad;
@@ -33,9 +40,12 @@ public class OnScreenButton {
     OnScreenButtonSpawn buttonTrade;
     OnScreenButtonSpawn buttonTradePressed;
 
+  //Alternative way of rendering images; - declares 5 different images.
     Image chooseOre, chooseClay, chooseWood, chooseWool, chooseWheat;
+  //Arrow for trading
     Image tradeArrowDown;
 	
+  //Booleans to control if a button has been pressed.
 	boolean buttonRoadControl = false;
 	boolean buttonHouseControl = false;
 	boolean buttonCityControl = false;
@@ -44,22 +54,29 @@ public class OnScreenButton {
     boolean buttonDevCardControl = false;
     boolean buttonDeselectControl = false;
 	
+  //The height of the buttons in the game in pixels
 	int buttonHeight = 56;
 	int buttonWidth = 97;
 
+	//The spacing between different buttons
     int xResourceSpacing = 55;
     int chooseResourcePosX = 680;
     int chooseResourcePosY = 635;
-	
+    int buttonSpacing = 65;
+    
+  //Ints to declare the starting position of buttons
 	int buttonStartPosX = screenWidth-buttonWidth-20;
 	int buttonStartPosY = screenHeight-buttonHeight-20;
-	int buttonSpacing = 65;
+	
+	//Int to check whose turn it is.
 	int playerTurn;
 	
 	OnScreenButton (Controller control) throws SlickException {
 		
 		this.control = control;
 		trade = new Trade(control);
+		
+		//Sets the instances of the OnScreenButtonSpawn equal to the position (X,Y) and what number it is.
 		buttonPlaceHouse = new OnScreenButtonSpawn(buttonStartPosX - buttonWidth - 9, buttonStartPosY, 7);
 		buttonPlaceCity  = new OnScreenButtonSpawn(buttonStartPosX, buttonStartPosY, 1);
 		buttonPlaceRoad  = new OnScreenButtonSpawn(buttonStartPosX, buttonStartPosY - buttonSpacing, 2);
@@ -75,6 +92,7 @@ public class OnScreenButton {
 	    buttonTrade = new OnScreenButtonSpawn(buttonStartPosX - buttonWidth - 9, buttonStartPosY - (buttonSpacing * 3), 13);
 	    buttonTradePressed = new OnScreenButtonSpawn(buttonStartPosX - buttonWidth - 9, buttonStartPosY - (buttonSpacing * 3), 14);
 	    
+	  //Reference where the image is located.
 	    chooseOre = new Image("images/ore.png");
         chooseClay = new Image("images/clay.png");
         chooseWood = new Image("images/wood.png");
@@ -84,10 +102,16 @@ public class OnScreenButton {
         tradeArrowDown = new Image("images/arrowDown.png");
 	}
 
+	/*
+	 * The order in which the different images are being render is important to
+	 * be sure on what images are being rendered first and last.
+	 */
 	public void render(GameContainer gc, Graphics g) throws SlickException {
 		
+		//Sets the playerturn equal to what has been received by the server from the PlayerInformation class.
 		playerTurn = game.client.obj.playerTurn;
 		
+		//Renders some of the buttons
 		buttonPlaceHouse.render(gc, g);
 		buttonPlaceCity.render(gc, g);
 		buttonPlaceRoad.render(gc, g);
@@ -102,6 +126,11 @@ public class OnScreenButton {
 			buttonDeselectPressed.render(gc, g);
 		}
         
+		/*
+         * Checks to see if the deselect button has been pressed
+         * Depending on this it will display one of two images; one grey and one color as
+         * indication to which is true.
+         */
 		if (control.tradeButtonControl == true || control.playerNo != game.client.obj.playerTurn) {
 			buttonTradePressed.render(gc, g);
 			g.drawImage(tradeArrowDown, chooseResourcePosX-50, chooseResourcePosY-40);
@@ -110,6 +139,8 @@ public class OnScreenButton {
 			buttonTrade.render(gc, g);
 		}
 
+		//if the player receives either a excavation card or monopoly card
+        //Spawns different images with spacing between them
         if(control.receivedExCard > 0 || control.receivedMonoCard == true) {
             g.drawString("Choose your resource: ", 720,600);
             g.drawImage(chooseOre, chooseResourcePosX, chooseResourcePosY);
@@ -119,24 +150,26 @@ public class OnScreenButton {
             g.drawImage(chooseWheat, chooseResourcePosX+xResourceSpacing*4, chooseResourcePosY);
         }
 
+        //Renders the image that it is your turn and you can now end your turn.
         if(control.playerNo == game.client.obj.playerTurn){
             buttonYourTurn.render(gc, g);
             g.drawString(yourTurnString, 800, 447);
         }
-
-        
+      //String in top of screen to indication the rounds left till the game ends
 		g.drawString("Rounds left: " + game.client.obj.roundCount, 380, 6);
-		
 	}
 	
 	public void update(GameContainer gc, int i) throws SlickException, IOException {
-	
+		//Gets the playerTurn information off the PlayerInformation class
 		playerTurn = game.client.obj.playerTurn;
+		
+		//Calls the update of trade.
 		trade.update(gc, i);
+		
 		int xMousePos = Mouse.getX(); //gets x position of mouse
 		int yMousePos = Mouse.getY(); //gets y position of mouse
 		
-		Input input = gc.getInput();
+		Input input = gc.getInput(); //gets input from player
 		
 		int ySpacing= 55;
 		
@@ -219,12 +252,18 @@ public class OnScreenButton {
 			}
 
 		
-        //ExcavationCard
+			/*
+	         * ExcavationCard
+	         * Statements to control and check, if a player clicks within one of the images, that is rendered when
+	         * the player gets this card. It checks the most left X, the most right X, the most top Y and most low Y
+	         * and if the mouse is pressed within, let that player get a new resource of the type clicked on.
+	         * The player can choose two resources.
+	         */
         if(control.receivedExCard > 0) {
             if((xMousePos > 680 && xMousePos < 680 + 45) && (yMousePos < screenHeight - 635 && yMousePos > screenHeight - 635 - 45 )){
                 if(input.isMousePressed(0)) {
                 	game.client.obj.playerResource[control.playerNo-1][0]++;
-                    control.receivedExCard--;
+                    control.receivedExCard--; //The amount of choices you have are being reduced by one
                 }
             }
             if((xMousePos > 680 + xResourceSpacing && xMousePos < 680 + 45 + xResourceSpacing) && (yMousePos < screenHeight - 635 && yMousePos > screenHeight - 635 - 45 )){
@@ -253,20 +292,23 @@ public class OnScreenButton {
             }
         }
 
-        //Monopoly card
+        /*
+         * Monopoly card
+         * This is the same as above with the EX-Card, however, now the player selects a resource and steals
+         * it from the other players, gaining all the resources of the other players, whom looses these.
+         */
         if(control.receivedMonoCard == true) {
             if((xMousePos > 680 && xMousePos < 680 + 45) && (yMousePos < screenHeight - 635 && yMousePos > screenHeight - 635 - 45 )){
                 if(input.isMousePressed(0)) {
-                    // = to the other players resources incl ones own
-                    //other players resource must return to 0.
                 	for(int j = 0 ; j<4; j++){
                 		if(j != control.playerNo-1){
+                			//This will give the player all the resources of the other player (ore in this case) and set theirs equal to 0.
                 			game.client.obj.playerResource[control.playerNo-1][0] += game.client.obj.playerResource[j][0];
                         	game.client.obj.playerResource[j][0] = 0;
                 		}
                 	}
-                	game.client.sendData(game.client.obj);
-                    control.receivedMonoCard = false;
+                	game.client.sendData(game.client.obj); //sends the data.
+                    control.receivedMonoCard = false;  //the player no longer have the card
                 }
             }
             if((xMousePos > 680 + xResourceSpacing && xMousePos < 680 + 45 + xResourceSpacing) && (yMousePos < screenHeight - 635 && yMousePos > screenHeight - 635 - 45 )){
@@ -319,17 +361,18 @@ public class OnScreenButton {
             }
         }
 		
-		//ButtonHouse
+		//ButtonHouse - Checks to see if the player have clicked the house button
 		if((xMousePos > buttonStartPosX && xMousePos < buttonStartPosX+buttonWidth) && (yMousePos < screenHeight-buttonStartPosY && yMousePos > screenHeight-buttonStartPosY-buttonHeight)) {
 			if (input.isMouseButtonDown(0)) {
 					buttonRoadControl = false;
 					buttonCityControl = false;
 					buttonHouseControl = true;
-					control.deselectButtonControl = true;
+					control.deselectButtonControl = true;//if the player have pressed the button, he can now deselect the button.
 			}
 		}
 		
-		//Deselect
+		//Deselect -- checks to see if the player have clicked the deselect button
+		//Sets the other buttons equal to false = no other buttons are pressed.
 		if((xMousePos > buttonStartPosX - buttonWidth - 9 && xMousePos < (buttonStartPosX - buttonWidth - 9) + buttonWidth) && (yMousePos < screenHeight - buttonStartPosY + buttonSpacing*2 && yMousePos > screenHeight - buttonStartPosY + buttonSpacing*2 - buttonHeight)) {
 			if(input.isMouseButtonDown(0)) {
 				buttonRoadControl = false;
@@ -344,7 +387,7 @@ public class OnScreenButton {
 			}
 		}
 		
-		//ButtonCity
+		//ButtonCity Checks to see if the player have clicked the city button
 		if ((xMousePos > buttonStartPosX - buttonWidth - 9 && xMousePos < (buttonStartPosX - buttonWidth - 9) + buttonWidth) && (yMousePos < screenHeight - buttonStartPosY && yMousePos > screenHeight - buttonStartPosY - buttonHeight)) {
 			if (input.isMouseButtonDown(0)) {
 					buttonRoadControl = false;
@@ -354,7 +397,7 @@ public class OnScreenButton {
 			}
 		}
 			
-		//ButtonRoad
+		//ButtonRoad Checks to see if the player have clicked the road button
 		if((xMousePos > buttonStartPosX && xMousePos < buttonStartPosX+buttonWidth) && (yMousePos < screenHeight-buttonStartPosY+buttonSpacing && yMousePos > screenHeight-buttonStartPosY+buttonSpacing-buttonHeight)) {
 			if (input.isMouseButtonDown(0)) {
 					buttonRoadControl = true;
@@ -364,7 +407,7 @@ public class OnScreenButton {
 			}
 		}
 			
-		//ButtonDice
+		//ButtonDice Checks to see if the player have clicked the dice button
 			if((xMousePos > buttonStartPosX && xMousePos < buttonStartPosX+buttonWidth) && (yMousePos < screenHeight-buttonStartPosY+(buttonSpacing*2) && yMousePos > screenHeight-buttonStartPosY+(buttonSpacing*2)-buttonHeight)) {
 				if(input.isMousePressed(0)) {
 					control.diceButtonClicked = true;
@@ -374,7 +417,7 @@ public class OnScreenButton {
 				}
 			}
 		
-		// ButtonBuyDevCard
+		// ButtonBuyDevCard Checks to see if the player have clicked the card buy button
 			if(control.playerNo == game.client.obj.playerTurn){
 	        if ((xMousePos > buttonStartPosX - buttonWidth - 9 && xMousePos < (buttonStartPosX - buttonWidth - 9) + buttonWidth) && (yMousePos < screenHeight - buttonStartPosY + buttonSpacing && yMousePos > screenHeight - buttonStartPosY + buttonSpacing - buttonHeight)) {
 	            if (input.isMousePressed(0)) {
@@ -387,12 +430,18 @@ public class OnScreenButton {
 	        }
 			}
 			
-		//ButtonEndTurn
+		/*
+		 * ButtonEndTurn Checks to see if the player have clicked the endturn
+		 * button All the other buttons are returned to false. It sends this
+		 * information to the server, to let the next player in line know, that
+		 * it is know his turn. Upon the fourth player, it resets to player no. 1
+		 */
 		if ((xMousePos > buttonStartPosX && xMousePos < buttonStartPosX + buttonWidth)
 				&& (yMousePos < screenHeight - buttonStartPosY + buttonSpacing * 3
 				&& yMousePos > screenHeight - buttonStartPosY + buttonSpacing * 3 - buttonHeight)) {
 			if (input.isMousePressed(0)) {
 
+				//Sets other buttons equal to false
 				buttonRoadControl = false;
 				buttonCityControl = false;
 				buttonHouseControl = false;
@@ -400,15 +449,15 @@ public class OnScreenButton {
                 control.devCardButtonClicked = false;
 				
 				if (playerTurn == control.playerNo) {
+					
+					//If the player no is 1 and it is the players turn
 					if (playerTurn == 1 && playerTurn == control.playerNo) {
-						control.testint = 0;
-						control.clickOnce = 0;
-						game.client.obj.playerTurn = 2;
-						game.client.sendData(game.client.obj);
+						control.clickOnce = 0; //makes sure the player only clicks once.
+						game.client.obj.playerTurn = 2; //set the player turn equal to player no 2
+						game.client.sendData(game.client.obj); //sends the data to the server.
 					}
 
 					else if (playerTurn == 2 && playerTurn == control.playerNo) {
-						control.testint = 0;
 						control.clickOnce = 0;
 						game.client.obj.playerTurn = 3;
 						game.client.sendData(game.client.obj);
@@ -416,14 +465,12 @@ public class OnScreenButton {
 					}
 
 					else if (playerTurn == 3 && playerTurn == control.playerNo) {
-						control.testint = 0;
 						control.clickOnce = 0;
 						game.client.obj.playerTurn = 4;
 						game.client.sendData(game.client.obj);
 					}
 
 					else if (playerTurn == 4 && playerTurn == control.playerNo) {
-						control.testint = 0;
 						control.clickOnce = 0;
 						game.client.obj.playerTurn = 1;
 						game.client.obj.roundCount--;
