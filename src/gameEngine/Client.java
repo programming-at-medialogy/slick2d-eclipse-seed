80583c688ae422d3a1f6bab9e628a1be45fa09f7
Akvile Krikstaponyte <akvilik@yahoo.com>
2015-12-04 13:00:31 +0100

    One more player

--- src/gameEngine/Client.java
+++ src/gameEngine/Client.java
@@ -26,7 +26,8 @@ public class Client extends BasicGame {
 	static int radius;
 	Server server = new Server();
 	public Player player1 = new Player("Atlanta", server.assignRole());
-	
+	public Player player2 = new Player("Atlanta", server.assignRole());
+
 	Client(String gamename) {
 		super(gamename);
 	}
@@ -34,14 +35,21 @@ public class Client extends BasicGame {
 	@Override
 	public void init(GameContainer gc) throws SlickException {
 		circle = new Circle(circleWidth, circleHeight, radius);// drawing circle
-		
+
 		System.out.println("Player 1 role is: " + player1.getRole());
 		System.out.println("Player 1 is now in city: " + player1.currentCity.toString());
-		server.citiesOnBoard.get(5).setInfectionRate(4);
-		for(int i=0; i < 16; i++){
-			if(server.citiesOnBoard.get(i).getInfectionRate() != 0){
-				System.out.println(server.citiesOnBoard.get(i).getcityName()+ " was infected. It's current infection rate is " + server.citiesOnBoard.get(i).getInfectionRate());
-			}
+		// server.citiesOnBoard.get(5).setInfectionRate(4);
+		for (int i = 0; i < 16; i++) {
+			if (server.citiesOnBoard.get(i).getInfectionRate() != 0) {
+				System.out.println(
+						server.citiesOnBoard.get(i).getcityName() + " was infected. It's current infection rate is "
+								+ server.citiesOnBoard.get(i).getInfectionRate());
+			} else if (server.citiesOnBoard.get(i).getInfectionRate() == 0) {
+				System.out.println(
+						server.citiesOnBoard.get(i).getcityName() + " was not infected. It's current infection rate is "
+								+ server.citiesOnBoard.get(i).getInfectionRate());
+			} // ^prints out the infected cities and the rest of cities'
+				// infection rates and whether it was indected or not^
 		}
 	}
 
@@ -51,18 +59,55 @@ public class Client extends BasicGame {
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException {
-		Image fieldDirector = new Image("images/fieldDirector.png");
+
+		// Image fieldDirector = new Image("images/fieldDirector.png"); //unused
+		// Image quarantineSpecialist = new
+		// Image("images/quarantineSpecialist.png"); //unused
+		// need to create images for the 6 role cards
+		Image medic = new Image("images/medic.png");
+		Image fieldDirector = new Image("images/roleCard1.png");
+
+		Image dispatcher = new Image("images/dispatcher.png");
+		Image researcher = new Image("images/researcher.png");
+		Image scientist = new Image("images/scientist.png");
+		Image consistencyPlanner = new Image("images/consistencyPlanner.png");
+		Image operationsExpert = new Image("images/operationsExpert.png");
 		Image player1Img = new Image("images/player1.png");
 		Image player2Img = new Image("images/player2.png");
 		Image background = new Image("images/board.jpg");// background image
 		g.drawImage(background, 0, 0);// rendering background
-		//Draw a player according vity positio
-		
-		g.drawImage(player1Img, server.citiesOnBoard.get(server.findCity(player1.getCurrentCity())).getcityPosX() , server.citiesOnBoard.get(server.findCity(player1.getCurrentCity())).getcityPosY());
-		
-		//System.out.println(Server.citiesOnBoard.get(Server.findCity(player1.getCurrentCity())).getcityPosX());
-		g.drawImage(fieldDirector, 26, 700);
-		//g.drawImage(player2Img, 396, 354);
+		// Draw a player according vity positio !!!not complete!!!
+
+		g.drawImage(player1Img, server.citiesOnBoard.get(server.findCity(player1.getCurrentCity())).getcityPosX(),
+				server.citiesOnBoard.get(server.findCity(player1.getCurrentCity())).getcityPosY());
+
+		// Renders the assigned role card
+		if (player1.getRole() == "Medic") {
+			g.drawImage(medic, 26, 700);
+
+		} else if (player1.getRole() == "Dispatcher") {
+			g.drawImage(dispatcher, 26, 700);
+
+		} else if (player1.getRole() == "Researcher") {
+
+			g.drawImage(researcher, 26, 700);
+
+		} else if (player1.getRole() == "Scientist") {
+			g.drawImage(scientist, 26, 700);
+
+		} else if (player1.getRole() == "Consistency planner") {
+			g.drawImage(consistencyPlanner, 26, 700);
+
+		} else if (player1.getRole() == "Operations expert") {
+			g.drawImage(operationsExpert, 26, 700);
+		} else if (player1.getRole() == "Field Director") {
+			g.drawImage(fieldDirector, 26, 700);
+		}
+
+		// System.out.println(Server.citiesOnBoard.get(Server.findCity(player1.getCurrentCity())).getcityPosX());
+		// g.drawImage(fieldDirector, 26, 700);
+		g.drawImage(player2Img, server.citiesOnBoard.get(server.findCity(player1.getCurrentCity())).getcityPosX() - 20,
+				server.citiesOnBoard.get(server.findCity(player1.getCurrentCity())).getcityPosY());
 		g.setColor(Color.white);
 		g.fillOval(circleWidth, circleHeight, radius, radius);
 		g.draw(circle);
@@ -73,7 +118,6 @@ public class Client extends BasicGame {
 
 		Client client = new Client("Infect");
 
-
 		DatagramSocket sock = null;
 		int port = 9000;
 		String s;
@@ -83,7 +127,7 @@ public class Client extends BasicGame {
 
 			int maxFPS = 60;// initialising frame rate per second
 			AppGameContainer appgc;
-			appgc = new AppGameContainer(new Client("Simple Slick Game"));
+			appgc = new AppGameContainer(new Client("Pandemic"));
 			appgc.setTargetFrameRate(maxFPS);// setting fps
 			appgc.setDisplayMode(boardWidth, boardHight, false);
 			appgc.start();
@@ -132,4 +176,3 @@ public class Client extends BasicGame {
 		System.out.println(message);
 	}
 }
-
