8fc6f8fd1fd29dbf98e01fa27cb321c4ef65fedd
Kallabow <kallabow2@gmail.com>
2015-12-04 14:59:20 +0100

    Update ClientRunnable.java

--- src/gameEngine/ClientRunnable.java
+++ src/gameEngine/ClientRunnable.java
@@ -12,34 +12,34 @@ public class ClientRunnable implements Runnable {
   protected Server server = null;
 
   public ClientRunnable(Socket clientSocket, Server server){
-        System.out.println("debil");
+        System.out.println("tst");
         
     this.server = server;
     this.clientSocket = clientSocket;
   }
 
   public void run() {
-    System.out.println("Lox");
+    System.out.println("test1");
     
     BufferedReader bufferedReader = null;
 	try {
-    System.out.println("Lox2");
+    System.out.println("test2");
 		bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 	} catch (IOException e1) {
-	      System.out.println("Lox3");
+	      System.out.println("test3");
 		e1.printStackTrace();
 	}
 
     String inputLine;
 
   try {
-    System.out.println("Started reading shit");
+    System.out.println("Started reading");
 		while((inputLine = bufferedReader.readLine()) != null) {
       server.sendMessage(clientSocket);
-      System.out.println("labas");
+      System.out.println("test4");
 		  System.out.println(inputLine);
     }
-    System.out.println("Shit is done");
+    System.out.println("Done");
 	} catch (IOException e) {
 		
 		e.printStackTrace();
