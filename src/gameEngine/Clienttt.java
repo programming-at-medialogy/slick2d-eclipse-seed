9804f4f4d2679896b81322dd6f70932ee23eb5a7
Kallabow <kallabow2@gmail.com>
2015-12-04 15:08:09 +0100

    Create Clienttt.java
    Def not final

---
+++ src/gameEngine/Clienttt.java
@@ -0,0 +1,36 @@
+package gameEngine;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Clienttt
+{
+	static Socket socket;
+	static DataInputStream in;
+	static DataOutputStream out;
+
+	public static void main (String[] args) throws Exception
+    {
+		System.out.println("Connecting..");
+		socket = new Socket("localhost", 9000);
+		System.out.println("Connected");
+		in = new DataInputStream(socket.getInputStream());
+		out = new DataOutputStream(socket.getOutputStream());
+		
+		out.write(System.getProperty("line.separator").getBytes());
+		Input input = new Input(in);
+		Thread thread = new Thread(input);
+		thread.start();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your name and hit 'Enter'...");
+		String name = scanner.nextLine();
+		out.writeUTF(name);
+
+		while(true) {
+			String sendMessage = scanner.nextLine();
+			out.writeUTF(sendMessage);
+		}
+
+    }
+
+}
